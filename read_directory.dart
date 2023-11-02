2dcfc7d6758639df8381d8f68b82b27b64c6738a
a3uhammad <hammadabu2002@gmail.com>
2023-11-02 15:45:39 +0530

    First Commit

---
+++ read_directory.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+void main() {
+  final directoryPath = 'E:\\root';
+  
+  final directory = Directory(directoryPath);
+
+  if (!directory.existsSync()) {
+    print('Directory does not exist.');
+    return;
+  }
+
+  printFilesRecursively(directory);
+}
+
+void printFilesRecursively(Directory directory) {
+  String currentFolderName = directory.uri.pathSegments.last;
+  
+  if (currentFolderName == '' || currentFolderName == '/') {
+    currentFolderName = directory.uri.pathSegments[directory.uri.pathSegments.length - 2];
+  }
+
+  for (var entity in directory.listSync()) {
+    if (entity is File) {
+      print('$currentFolderName - ${entity.uri.pathSegments.last}');
+    }
+  }
+  
+  for (var entity in directory.listSync()) {
+    if (entity is Directory) {
+      printFilesRecursively(entity);
+    }
+  }
+}
